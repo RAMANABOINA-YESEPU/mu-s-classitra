b182f4d3f167975360888f1136f6524c7946a3d8
RAMANABOINA-YESEPU <ramanaboinayesepu8256@gmail.com>
2026-02-22 12:36:32 +0530

    Configure Vite PWA for robust offline mobile caching

--- vite.config.js
+++ vite.config.js
@@ -30,6 +30,43 @@ export default defineConfig({
             type: 'image/jpeg',
             purpose: 'any maskable'
           }
+        ],
+        display: 'standalone',
+        background_color: '#0f172a',
+        start_url: '/',
+        orientation: 'portrait'
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          },
+          {
+            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'gstatic-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          }
         ]
       }
     })
